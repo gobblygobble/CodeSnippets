92af3256675b0fa5dfaeae23ca84fdbb5e502a9a
gobblygobble <gobblygobble@kaist.ac.kr>
2021-02-08 15:37:06 +0900

    '2021.02.08 'git-updater: complete''

---
+++ shell-script/git-updater.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# get date: ex. 1970.01.01
+#now=$(LANG=en_us_88591; date "+%Y.%m.%d")
+
+# get full date : ex. Thurs Jan 1 00:00:00 KST 1970
+#now=$(LANG=en_us_88591; date)
+
+# get date + time: ex. 1970.01.01 00:00:00
+now=$(LANG=en_us_88591; date "+%Y.%m.%d %T")
+#echo $now
+
+# get ALL user inputs
+message="'$*'"
+
+# git commit with date + message
+#git commit -m $today" "$message
+git commit -m "'$today $message'"
+#git push origin master
